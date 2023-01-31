a057fe2e8730a862fa16845393b3a88465436c2a
Vinita Yadav <yadav.vinita@gmail.com>
2023-01-31 17:27:54 +0000

    added all the elements using bootstrap row and col

---
+++ assests/js/script.js
@@ -0,0 +1,20 @@
+//
+$(document).ready(function(){
+
+//display current day, date an time
+var now = moment().format('dddd Do MMMM YYYY, h:mm:ss a');
+$("#currentDay").text(now);
+let hourNow = moment().format('h');
+console.log(hourNow);
+
+//create timeblock rows 
+
+
+
+
+
+
+
+
+
+})
