98b3dd32c8ebe02e4603fd9f66448bfd86498b6f
Vinita Yadav <yadav.vinita@gmail.com>
2023-02-01 11:42:02 +0000

    create func to compare current time with timediv

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,13 +1,36 @@
 //
 $(document).ready(function(){
 
-//display current day, date an time
-var now = moment().format('dddd Do MMMM YYYY, h:mm:ss a');
+//display current day, date and time
+const now = moment().format('dddd Do MMMM YYYY, h:mm:ss a');
 $("#currentDay").text(now);
-let hourNow = moment().format('h');
-console.log(hourNow);
 
-//create timeblock rows 
+let timeNow = moment().format('h');
+console.log(timeNow);
+
+//compare each row of time with the cureent time
+//Accordingly set the class to present, past or future
+$(".time-label").each(function() {
+    var timeDiv = $(this).attr("id");
+    // console.log(timeDiv);
+    if (timeNow === timeDiv) {
+        // $(this).addClass('present');
+        $(this).children('.description').addClass('present');
+
+    } else if (timeNow < timeDiv) {
+        $(this).children('.description').addClass('future');
+
+    } else if (timeNow > timeDiv) {
+        $(this).children('.description').addClass('past');
+    }
+});
+
+//function to save the schedules to local storage
+$('.saveBtn').click(function(event) {
+    event.preventDefault();
+    
+}
+) 
 
 
 
