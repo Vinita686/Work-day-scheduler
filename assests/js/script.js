28992e73c831c57614fc857523714a4f5b81fcf8
Vinita Yadav <yadav.vinita@gmail.com>
2023-02-01 14:07:25 +0000

    fetch and persist added tasks from localstorage

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,16 +5,18 @@ $(document).ready(function(){
 const now = moment().format('dddd Do MMMM YYYY, h:mm:ss a');
 $("#currentDay").text(now);
 
-let timeNow = moment().format('h');
+let timeNow = moment().format('HH');
 console.log(timeNow);
 
 //compare each row of time with the cureent time
 //Accordingly set the class to present, past or future
 $(".time-label").each(function() {
     var timeDiv = $(this).attr("id");
-    // console.log(timeDiv);
+    console.log(timeDiv);
     if (timeNow === timeDiv) {
+        $(this).addClass('present');
         $(this).children('.description').addClass('present');
+
 
     } else if (timeNow < timeDiv) {
         $(this).children('.description').addClass('future');
@@ -34,12 +36,15 @@ $('.saveBtn').click(function(event) {
     // console.log(time);
 });
 
-
-
-
-
-
-
-
+//fecthing persistent added tasks
+$('#09 .description').val(localStorage.getItem('09'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
 
 })
